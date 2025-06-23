b5d8fcd1204171c40b4943d0d4690fe4f568ff33
Alex Casalboni <alex@edgee.cloud>
2025-06-23 17:37:35 +0200

    add initial implementation

---
+++ src/lib.rs
@@ -0,0 +1,243 @@
+use crate::exports::edgee::components::data_collection::{
+    Data, Dict, EdgeeRequest, Event, HttpMethod,
+};
+use anyhow::Context;
+use exports::edgee::components::data_collection::Guest;
+use std::collections::HashMap;
+use woopra_payload::{WoopraPayloadIdentify, WoopraPayloadTrack};
+
+mod woopra_payload;
+
+wit_bindgen::generate!({world: "data-collection", path: ".edgee/wit", generate_all});
+export!(Component);
+
+struct Component;
+
+const WOOPRA_HOST: &str = "https://www.woopra.com";
+const WOOPRA_TRACK_ENDPOINT: &str = "/track/ce";
+const WOOPRA_IDENTIFY_ENDPOINT: &str = "/track/identify";
+
+impl Guest for Component {
+    fn page(edgee_event: Event, settings_dict: Dict) -> Result<EdgeeRequest, String> {
+        if let Data::Page(ref data) = edgee_event.data {
+            let settings = Settings::new(settings_dict).map_err(|e| e.to_string())?;
+
+            let mut payload =
+                WoopraPayloadTrack::new(&edgee_event, settings.project_name, "pv".to_string())
+                    .map_err(|e| e.to_string())?;
+
+            payload.add_page_properties(data);
+
+            let querystring = serde_qs::to_string(&payload).map_err(|e| e.to_string())?;
+
+            Ok(
+                build_edgee_request(querystring, WOOPRA_TRACK_ENDPOINT.to_string())
+                    .map_err(|e| e.to_string())?,
+            )
+        } else {
+            Err("Missing page data".to_string())
+        }
+    }
+
+    fn track(edgee_event: Event, settings_dict: Dict) -> Result<EdgeeRequest, String> {
+        if let Data::Track(ref data) = edgee_event.data {
+            if data.name.is_empty() {
+                return Err("Track is not set".to_string());
+            }
+
+            let settings = Settings::new(settings_dict).map_err(|e| e.to_string())?;
+
+            let mut payload =
+                WoopraPayloadTrack::new(&edgee_event, settings.project_name, data.name.clone())
+                    .map_err(|e| e.to_string())?;
+
+            payload.add_track_properties(data);
+
+            let querystring = serde_qs::to_string(&payload).map_err(|e| e.to_string())?;
+
+            Ok(
+                build_edgee_request(querystring, WOOPRA_TRACK_ENDPOINT.to_string())
+                    .map_err(|e| e.to_string())?,
+            )
+        } else {
+            Err("Missing track data".to_string())
+        }
+    }
+
+    fn user(edgee_event: Event, settings_dict: Dict) -> Result<EdgeeRequest, String> {
+        if let Data::User(ref data) = edgee_event.data {
+            let settings = Settings::new(settings_dict).map_err(|e| e.to_string())?;
+
+            let mut payload = WoopraPayloadIdentify::new(&edgee_event, settings.project_name)
+                .map_err(|e| e.to_string())?;
+
+            payload.add_user_properties(data);
+
+            let querystring = serde_qs::to_string(&payload).map_err(|e| e.to_string())?;
+
+            Ok(
+                build_edgee_request(querystring, WOOPRA_IDENTIFY_ENDPOINT.to_string())
+                    .map_err(|e| e.to_string())?,
+            )
+        } else {
+            Err("Missing track data".to_string())
+        }
+    }
+}
+
+fn build_edgee_request(querystring: String, endpoint: String) -> anyhow::Result<EdgeeRequest> {
+    let headers = vec![(String::from("content-length"), String::from("0"))];
+
+    Ok(EdgeeRequest {
+        method: HttpMethod::Get,
+        url: format!("{}{}?{}", WOOPRA_HOST, endpoint, querystring),
+        headers,
+        forward_client_headers: true,
+        body: String::new(),
+    })
+}
+
+pub struct Settings {
+    pub project_name: String,
+}
+
+impl Settings {
+    pub fn new(settings_dict: Dict) -> anyhow::Result<Self> {
+        let settings_map: HashMap<String, String> = settings_dict
+            .iter()
+            .map(|(key, value)| (key.to_string(), value.to_string()))
+            .collect();
+
+        let project_name = settings_map
+            .get("project_name")
+            .context("Missing example setting")?
+            .to_string();
+
+        Ok(Self { project_name })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use crate::exports::edgee::components::data_collection::{
+        Campaign, Client, Context, Data, EventType, PageData, Session, UserData,
+    };
+    use exports::edgee::components::data_collection::Consent;
+    use pretty_assertions::assert_eq;
+    use uuid::Uuid;
+
+    fn sample_user_data(edgee_id: String) -> UserData {
+        UserData {
+            user_id: "123".to_string(),
+            anonymous_id: "456".to_string(),
+            edgee_id,
+            properties: vec![
+                ("prop1".to_string(), "value1".to_string()),
+                ("prop2".to_string(), "10".to_string()),
+            ],
+        }
+    }
+
+    fn sample_context(edgee_id: String, locale: String, session_start: bool) -> Context {
+        Context {
+            page: sample_page_data(),
+            user: sample_user_data(edgee_id),
+            client: Client {
+                city: "Paris".to_string(),
+                ip: "192.168.0.1".to_string(),
+                locale,
+                timezone: "CET".to_string(),
+                user_agent: "Chrome".to_string(),
+                user_agent_architecture: "x86".to_string(),
+                user_agent_bitness: "64".to_string(),
+                user_agent_full_version_list: "abc".to_string(),
+                user_agent_version_list: "abc".to_string(),
+                user_agent_mobile: "mobile".to_string(),
+                user_agent_model: "don't know".to_string(),
+                os_name: "MacOS".to_string(),
+                os_version: "latest".to_string(),
+                screen_width: 1024,
+                screen_height: 768,
+                screen_density: 2.0,
+                continent: "Europe".to_string(),
+                country_code: "FR".to_string(),
+                country_name: "France".to_string(),
+                region: "West Europe".to_string(),
+            },
+            campaign: Campaign {
+                name: "random".to_string(),
+                source: "random".to_string(),
+                medium: "random".to_string(),
+                term: "random".to_string(),
+                content: "random".to_string(),
+                creative_format: "random".to_string(),
+                marketing_tactic: "random".to_string(),
+            },
+            session: Session {
+                session_id: "random".to_string(),
+                previous_session_id: "random".to_string(),
+                session_count: 2,
+                session_start,
+                first_seen: 123,
+                last_seen: 123,
+            },
+        }
+    }
+
+    fn sample_page_data() -> PageData {
+        PageData {
+            name: "page name".to_string(),
+            category: "category".to_string(),
+            keywords: vec!["value1".to_string(), "value2".into()],
+            title: "page title".to_string(),
+            url: "https://example.com/full-url?test=1".to_string(),
+            path: "/full-path".to_string(),
+            search: "?test=1".to_string(),
+            referrer: "https://example.com/another-page".to_string(),
+            properties: vec![
+                ("prop1".to_string(), "value1".to_string()),
+                ("prop2".to_string(), "10".to_string()),
+                ("currency".to_string(), "USD".to_string()),
+            ],
+        }
+    }
+
+    fn sample_page_event(
+        consent: Option<Consent>,
+        edgee_id: String,
+        locale: String,
+        session_start: bool,
+    ) -> Event {
+        Event {
+            uuid: Uuid::new_v4().to_string(),
+            timestamp: 123,
+            timestamp_millis: 123,
+            timestamp_micros: 123,
+            event_type: EventType::Page,
+            data: Data::Page(sample_page_data()),
+            context: sample_context(edgee_id, locale, session_start),
+            consent,
+        }
+    }
+
+    #[test]
+    fn page_works_fine() {
+        let event = sample_page_event(
+            Some(Consent::Granted),
+            "abc".to_string(),
+            "fr".to_string(),
+            true,
+        );
+        let settings = vec![("your-credentials".to_string(), "abc".to_string())];
+        let result = Component::page(event, settings);
+
+        assert_eq!(result.is_err(), false);
+        let edgee_request = result.unwrap();
+        assert_eq!(edgee_request.method, HttpMethod::Post);
+        assert_eq!(edgee_request.body.is_empty(), true);
+        assert_eq!(edgee_request.url.starts_with("https://example.com/"), true);
+        // add more checks (headers, querystring, etc.)
+    }
+}
---
+++ src/woopra_payload.rs
@@ -0,0 +1,307 @@
+use serde::Serialize;
+use std::collections::HashMap;
+
+use crate::exports::edgee::components::data_collection::Event;
+
+#[derive(Serialize, Debug, Default)]
+pub(crate) struct WoopraPayloadTrack {
+    project: String,
+    event: String,
+    timestamp: String,
+
+    #[serde(
+        skip_serializing_if = "HashMap::is_empty",
+        serialize_with = "serialize_cv_prefixed",
+        flatten
+    )]
+    visitor_properties: HashMap<String, String>,
+    #[serde(
+        skip_serializing_if = "HashMap::is_empty",
+        serialize_with = "serialize_ce_prefixed",
+        flatten
+    )]
+    event_properties: HashMap<String, String>,
+    #[serde(
+        skip_serializing_if = "HashMap::is_empty",
+        serialize_with = "serialize_cs_prefixed",
+        flatten
+    )]
+    session_properties: HashMap<String, String>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    screen: Option<String>, // e.g. "1920x1080"
+    #[serde(skip_serializing_if = "Option::is_none")]
+    language: Option<String>, // e.g. "en-US"
+    #[serde(skip_serializing_if = "Option::is_none")]
+    referer: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    timeout: Option<String>, // won't use for now (default is 30000)
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    browser: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    os: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    device: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    ip: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    app: Option<String>,
+}
+impl WoopraPayloadTrack {
+    pub(crate) fn new(edgee_event: &Event, project: String, event: String) -> anyhow::Result<Self> {
+        let mut payload = WoopraPayloadTrack {
+            event,
+            project,
+            app: Some("Edgee".to_string()),
+            timestamp: edgee_event.timestamp.to_string(),
+            ..WoopraPayloadTrack::default()
+        };
+
+        // context.page stuff
+        payload.add_page_properties(&edgee_event.context.page);
+
+        if !edgee_event.context.client.locale.is_empty() {
+            payload.language = Some(edgee_event.context.client.locale.clone());
+        }
+
+        if !edgee_event
+            .context
+            .client
+            .user_agent_full_version_list
+            .is_empty()
+        {
+            payload.browser = Some(
+                edgee_event
+                    .context
+                    .client
+                    .user_agent_full_version_list
+                    .clone(),
+            );
+        }
+
+        if !edgee_event.context.client.os_name.is_empty() {
+            if !edgee_event.context.client.os_version.is_empty() {
+                payload.os = Some(format!(
+                    "{} {}",
+                    edgee_event.context.client.os_name.clone(),
+                    edgee_event.context.client.os_version.clone(),
+                ));
+            } else {
+                payload.os = Some(edgee_event.context.client.os_name.clone());
+            }
+        }
+
+        if edgee_event.context.client.screen_width.is_positive()
+            && edgee_event.context.client.screen_height.is_positive()
+        {
+            payload.screen = Some(format!(
+                "{:?}x{:?}",
+                edgee_event.context.client.screen_width.clone(),
+                edgee_event.context.client.screen_height.clone()
+            ));
+        }
+
+        // user
+        if !edgee_event.context.user.anonymous_id.is_empty() {
+            payload.visitor_properties.insert(
+                "anonymous_id".to_string(),
+                edgee_event.context.user.anonymous_id.clone(),
+            );
+        }
+        if !edgee_event.context.user.user_id.is_empty() {
+            payload.visitor_properties.insert(
+                "user_id".to_string(),
+                edgee_event.context.user.user_id.clone(),
+            );
+        }
+
+        // user properties
+        if !edgee_event.context.user.properties.is_empty() {
+            for (key, value) in edgee_event.context.user.properties.clone().iter() {
+                payload
+                    .visitor_properties
+                    .insert(key.to_string(), value.clone());
+            }
+        }
+
+        // geo ip
+        if !edgee_event.context.client.country_code.is_empty() {
+            payload.visitor_properties.insert(
+                "country".to_string(),
+                edgee_event.context.client.country_code.clone(),
+            );
+        }
+
+        // ip address
+        if !edgee_event.context.client.ip.is_empty() {
+            payload.ip = Some(edgee_event.context.client.ip.clone());
+        }
+
+        // session
+        payload.session_properties.insert(
+            "session_id".to_string(),
+            edgee_event.context.session.session_id.clone(),
+        );
+        payload.session_properties.insert(
+            "session_count".to_string(),
+            edgee_event
+                .context
+                .session
+                .session_count
+                .clone()
+                .to_string(),
+        );
+
+        Ok(payload)
+    }
+
+    // this method can be used to add page properties to the payload (from event.data or context.page)
+    pub(crate) fn add_page_properties(
+        &mut self,
+        page: &crate::exports::edgee::components::data_collection::PageData,
+    ) {
+        if !page.title.is_empty() {
+            self.event_properties
+                .insert("title".to_string(), page.title.clone());
+        }
+        if !page.url.is_empty() {
+            let uri = format!("{}{}", page.url.clone(), page.search.clone());
+            self.event_properties.insert("uri".to_string(), uri);
+        }
+        if !page.referrer.is_empty() {
+            self.referer = Some(page.referrer.clone());
+        }
+        for (key, value) in page.properties.iter() {
+            let key = key.replace(" ", "_");
+            self.event_properties
+                .insert(format!("page_{}", key), value.to_string());
+        }
+    }
+
+    // this method can be used to add user properties to the payload (from event.data or context.user)
+    pub(crate) fn add_track_properties(
+        &mut self,
+        data: &crate::exports::edgee::components::data_collection::TrackData,
+    ) {
+        // track data properties
+        if !data.properties.is_empty() {
+            for (key, value) in data.properties.clone().iter() {
+                self.event_properties
+                    .insert(key.to_string(), value.to_string());
+            }
+        }
+    }
+}
+
+#[derive(Serialize, Debug, Default)]
+pub(crate) struct WoopraPayloadIdentify {
+    project: String,
+
+    #[serde(
+        skip_serializing_if = "HashMap::is_empty",
+        serialize_with = "serialize_cv_prefixed",
+        flatten
+    )]
+    visitor_properties: HashMap<String, String>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    cv_id: Option<String>, // default identifier (could be cv_email too)
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    cookie: Option<String>, // optional cookie id (required only if no other identifier is provided)
+}
+impl WoopraPayloadIdentify {
+    pub(crate) fn new(edgee_event: &Event, project: String) -> anyhow::Result<Self> {
+        let mut payload = WoopraPayloadIdentify {
+            project,
+            ..WoopraPayloadIdentify::default()
+        };
+
+        // context.user stuff
+        payload.add_user_properties(&edgee_event.context.user);
+
+        // geo ip
+        if !edgee_event.context.client.country_code.is_empty() {
+            payload.visitor_properties.insert(
+                "country".to_string(),
+                edgee_event.context.client.country_code.clone(),
+            );
+        }
+
+        Ok(payload)
+    }
+
+    // this method can be used to add user properties to the payload (from event.data or context.user)
+    pub(crate) fn add_user_properties(
+        &mut self,
+        user: &crate::exports::edgee::components::data_collection::UserData,
+    ) {
+        if !user.anonymous_id.is_empty() {
+            self.cv_id = Some(user.anonymous_id.clone());
+        }
+        if !user.user_id.is_empty() {
+            // overwrite the anonymous_id with user_id if available
+            self.cv_id = Some(user.user_id.clone());
+        }
+
+        // user properties
+        if !user.properties.is_empty() {
+            for (key, value) in user.properties.clone().iter() {
+                self.visitor_properties
+                    .insert(key.to_string(), value.clone());
+            }
+        }
+    }
+}
+
+// Helper function to serialize HashMap with "cv_" prefix
+fn serialize_cv_prefixed<S>(map: &HashMap<String, String>, serializer: S) -> Result<S::Ok, S::Error>
+where
+    S: serde::Serializer,
+{
+    let mut prefixed_map: HashMap<String, String> = HashMap::new();
+    for (key, value) in map.iter() {
+        let prefixed_key = if key.starts_with("cv_") {
+            key.clone()
+        } else {
+            format!("cv_{}", key)
+        };
+        prefixed_map.insert(prefixed_key, value.clone());
+    }
+    prefixed_map.serialize(serializer)
+}
+
+// Helper function to serialize HashMap with "ce_" prefix
+fn serialize_ce_prefixed<S>(map: &HashMap<String, String>, serializer: S) -> Result<S::Ok, S::Error>
+where
+    S: serde::Serializer,
+{
+    let mut prefixed_map: HashMap<String, String> = HashMap::new();
+    for (key, value) in map.iter() {
+        let prefixed_key = if key.starts_with("cv_") {
+            key.clone()
+        } else {
+            format!("ce_{}", key)
+        };
+        prefixed_map.insert(prefixed_key, value.clone());
+    }
+    prefixed_map.serialize(serializer)
+}
+
+// Helper function to serialize HashMap with "ce_" prefix
+fn serialize_cs_prefixed<S>(map: &HashMap<String, String>, serializer: S) -> Result<S::Ok, S::Error>
+where
+    S: serde::Serializer,
+{
+    let mut prefixed_map: HashMap<String, String> = HashMap::new();
+    for (key, value) in map.iter() {
+        let prefixed_key = if key.starts_with("cv_") {
+            key.clone()
+        } else {
+            format!("cs_{}", key)
+        };
+        prefixed_map.insert(prefixed_key, value.clone());
+    }
+    prefixed_map.serialize(serializer)
+}
